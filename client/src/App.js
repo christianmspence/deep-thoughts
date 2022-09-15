9aa7c35d5e5f1f7c74c3c97be3996c3521ae2bbb
christianmspence <103942566+christianmspence@users.noreply.github.com>
2022-09-15 14:14:54 -0400

    login and signup routes and auth set up

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { setContext } from '@apollo/client/link/context';
+
 
 import React from 'react';
 
@@ -18,8 +20,18 @@ const httpLink = createHttpLink({
   uri: 'http://localhost:3001/graphql',
 });
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
